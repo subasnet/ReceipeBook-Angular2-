028346ebf5ba062314694842eb548805a161de2d
Subash Basnet <subashbasnet@Subashs-iMac.local>
2017-02-15 10:15:55 +0200

    header and receipe component added

---
+++ src/app/receipes/index.ts
@@ -0,0 +1,1 @@
+export { ReceipesComponent } from './receipes.component';
---
+++ src/app/receipes/receipes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-receipes',
+  templateUrl: './receipes.component.html',
+})
+export class ReceipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
